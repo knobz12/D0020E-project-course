0231341d18fefe93e8d976022d2a66a626bafcc1
Knobz <mjojla@hotmail.com>
2023-11-20 10:06:05 +0100

    Updated json parsing function

--- pptxreader/JsonFunction.py
+++ pptxreader/JsonFunction.py
@@ -1,10 +1,26 @@
 import json
 import time
 
+class FillJson():
+    def __init__(self) -> None:
+        self.template = {}
 
-with open("Test_json/test.json", "r") as f:
-    x = json.loads(f.read())
+    def open_json_from_file(self, file):
+        with open(file, "r") as file:
+            json_file = json.loads(file.read())
+        self.template = json_file
 
 
-with open("Test_json/best.json", "w") as f:
-    f.write(json.dumps(x))
+    def write_json_to_file(self):
+        with open("Test_json/best.json", "w") as f:
+            f.write(json.dumps(self.template))
+
+    def return_json_keys(self):
+        keys = []
+        for values in self.template.keys():
+            keys.append(values)
+        return keys
+
+
+    def return_json(self):
+        return self.template
