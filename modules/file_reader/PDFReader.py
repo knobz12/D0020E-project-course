3fe7114f4f4c2200bcd26c9c8cfaa407a299cef1
Jonathan Ekberg <jodrim@gmail.com>
2023-11-20 14:05:48 +0100

    PDF image to text

--- modules/file_reader/PDFReader.py
+++ modules/file_reader/PDFReader.py
@@ -15,3 +15,22 @@ class PDFToText():
     
     def ConvertImageToText(self, file):
         pass
+
+    def ConvertToImage(self, file):
+        extracted_text = ""
+        with open(file, 'rb') as PDF:
+            pdfReader = PyPDF2.PdfReader(PDF)
+            for i in range(len(pdfReader.pages)):
+                pageObj = pdfReader.pages[i]
+                for image in pageObj.images:
+                    import io
+                    import pytesseract
+                    from PIL import Image
+
+                    img = Image.open(io.BytesIO(image.data))
+                    img.save(f"./{image.name}")
+                    scan = pytesseract.image_to_string(img)
+                    print(f"Scan of {image.name} ({len(scan)}):",scan)
+                    if len(scan) > 0:
+                        extracted_text += scan + "\n\n"
+        return extracted_text
