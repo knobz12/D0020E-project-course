778bcf610fcdc3b33666ae03dcf9d9dd9a87e21a
Knobz <mjojla@hotmail.com>
2023-11-20 11:51:49 +0100

    Auto stash before merge of "main" and "origin/main"

--- modules/Metadata.py
+++ modules/Metadata.py
@@ -1,3 +1,12 @@
 import os
 
-os.path.getmtime(path)
+
+class MetaData():
+    def __init__(self) -> None:
+        pass
+
+    def GetModifiedDate(self, file):
+        return os.path.getmtime(file)
+    
+p = MetaData()
+print(p.GetModifiedDate("tests/sample_files/Test_txt/t.txt"))
