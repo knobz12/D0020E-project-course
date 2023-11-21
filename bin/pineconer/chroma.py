0e7051a1039fc23aa078e74a374e0ea9bf526559
Jonathan Ekberg <jodrim@gmail.com>
2023-11-21 14:28:04 +0100

    Fix chroma code

--- bin/pineconer/chroma.py
+++ bin/pineconer/chroma.py
@@ -30,9 +30,9 @@ vectorstore = Chroma(embedding_function=embedding_function,client=client,collect
 # result = db.similarity_search("CNN when switching GTX gpu")
 # print(result)
 
-import pathlib
-dataset_file_path = pathlib.Path("./result.jsonl")
 def upsert_data() -> None:
+    import pathlib
+    dataset_file_path = pathlib.Path("./result.jsonl")
     print("Loading dataset...")
     data = load_dataset("json", data_files=str(dataset_file_path.resolve()),split="train")
     print("Loaded dataset:")
@@ -49,77 +49,79 @@ def upsert_data() -> None:
         course = str(obj["course"])
         collection.add([ident],metadatas={'course':course,'text': text},documents=[text])
 
+def run_llm():
+    from langchain import hub
+    prompt = hub.pull("rlm/rag-prompt")
+    print(prompt)
+    from langchain.llms.llamacpp import LlamaCpp
+    from langchain.callbacks.manager import CallbackManager
+    from langchain.callbacks import StreamingStdOutCallbackHandler
+    # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    llm = LlamaCpp(
+        model_path="S:\models\llama-2-13b-chat.Q3_K_M.gguf",
+        # model_path="S:\models\llama-2-70b-chat.Q2_K.gguf",
+        n_gpu_layers=43,
+        n_batch=256,
+        use_mmap=True,
+        n_ctx=2048,
+        f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
+        # callback_manager=callback_manager,
+        verbose=True,
+    )
+
+    prompt_str = """Human: You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. 
+    If you don't know the answer, just say that you don't know. 
+    Use three sentences maximum and keep the answer concise. 
+    Don't directly refer to the context text, pretend like you already knew the context information.
+    Question: {question}
+    Context: {context}
+    Answer:"""
+
+    retriever = vectorstore.as_retriever(search_kwargs={'k': 2})
+    # import langchain
+    # langchain.debug = True
+    from langchain import hub
+    from langchain.prompts.prompt import Prompt
+    from langchain.schema.runnable import RunnablePassthrough
+    from langchain.schema.output_parser import StrOutputParser
+    prompt = hub.pull("rlm/rag-prompt")
+
+    from langchain.prompts import ChatPromptTemplate
+    prompt = ChatPromptTemplate.from_template(prompt_str)
+
+    # print(retriever.get_relevant_documents("CNN when switching GTX gpu"))
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    questions: list[str] = [
+        "How many workshops will ther be?",
+        "What will the projects be about?"
+        # "How many hidden units does P2NN have and how were they selected?",
+        # "For SMLP, what were the test errors?"
+    ]
+
+    import time
+    for (idx, question) in enumerate(questions):
+        # for chunk in rag_chain.stream("CNN when switching GTX gpu"):
+        print(f"Asking question {idx}")
+        for chunk in rag_chain.stream(question):
+            print(chunk, end="")
+            time.sleep(0.1)
+        print("\n")
+        print(f"Done with question {idx}")
+        time.sleep(1)
+
+    time.sleep(10)
+        # llm.
+    # rag_chain.strea
+
+# Upsert the result.jsonl data
 upsert_data()
 
-    # docs = collection.get(ids)
-    # print("docs")
-
-# collection.upsert()
-
-# from langchain import hub
-# prompt = hub.pull("rlm/rag-prompt")
-# print(prompt)
-# from langchain.llms.llamacpp import LlamaCpp
-# from langchain.callbacks.manager import CallbackManager
-# from langchain.callbacks import StreamingStdOutCallbackHandler
-# # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-
-# llm = LlamaCpp(
-#     model_path="S:\models\llama-2-13b-chat.Q3_K_M.gguf",
-#     # model_path="S:\models\llama-2-70b-chat.Q2_K.gguf",
-#     n_gpu_layers=43,
-#     n_batch=256,
-#     use_mmap=True,
-#     n_ctx=2048,
-#     f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
-#     # callback_manager=callback_manager,
-#     verbose=True,
-# )
-
-# prompt_str = """Human: You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. 
-# If you don't know the answer, just say that you don't know. 
-# Use three sentences maximum and keep the answer concise. 
-# Don't directly refer to the context text, pretend like you already knew the context information.
-# Question: {question}
-# Context: {context}
-# Answer:"""
-
-# retriever = vectorstore.as_retriever(search_kwargs={'k': 1})
-# # import langchain
-# # langchain.debug = True
-# from langchain import hub
-# from langchain.prompts.prompt import Prompt
-# from langchain.schema.runnable import RunnablePassthrough
-# from langchain.schema.output_parser import StrOutputParser
-# prompt = hub.pull("rlm/rag-prompt")
-
-# from langchain.prompts import ChatPromptTemplate
-# prompt = ChatPromptTemplate.from_template(prompt_str)
-
-# # print(retriever.get_relevant_documents("CNN when switching GTX gpu"))
-# rag_chain = (
-#     {"context": retriever, "question": RunnablePassthrough()}
-#     | prompt
-#     | llm
-#     | StrOutputParser()
-# )
-
-# questions: list[str] = [
-#     "How many hidden units does P2NN have and how were they selected?",
-#     "For SMLP, what were the test errors?"
-# ]
-
-# import time
-# for (idx, question) in enumerate(questions):
-#     # for chunk in rag_chain.stream("CNN when switching GTX gpu"):
-#     print(f"Asking question {idx}")
-#     for chunk in rag_chain.stream(question):
-#         print(chunk, end="")
-#         time.sleep(0.1)
-#     print("\n")
-#     print(f"Done with question {idx}")
-#     time.sleep(1)
-
-# time.sleep(10)
-#     # llm.
-# # rag_chain.strea
+# Run RAG with the dataset
+# run_llm()
