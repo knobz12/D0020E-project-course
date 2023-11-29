00a338c2f19b57f0a897b4218e712313215e4f87
Jonathan Ekberg <jodrim@gmail.com>
2023-11-29 11:21:46 +0100

    Add working streaming summmary from html

--- backend/modules/ai/summarizer.py
+++ backend/modules/ai/summarizer.py
@@ -31,6 +31,7 @@ Also end the summary with by adding "END" and start with "START"
 Text: {text}
 
 Answer:""".format(text = text)
+
         prompt_with_previous=  """Human: You are an assistant summarizing document text.
 Use the following pieces of retrieved context to add to the summary text. 
 If you can't add to it simply return the old.
@@ -70,6 +71,75 @@ Answer:""".format(summary = previous_summary,context=text)
     print("Original text:")
     print(texts)
     return summaryTrim
+
+from typing import Generator
+def summarize_doc_stream(id: str) -> Generator[str, str, None]:
+    llm = create_llm()
+    vectorstore = create_vectorstore()
+
+    docs = vectorstore.get(limit=100,include=["metadatas"],where={"id":id})
+    print(docs)
+    print("doc count:",len(docs['ids']))
+    results: list[str] = []
+    texts = ""
+    for (idx, meta) in enumerate(docs["metadatas"]):
+        text =meta["text"]
+        previous_summary: str | None = results[idx - 1] if idx > 1 else None
+
+        prompt = """Human: You are an assistant summarizing document text.
+I want you to summarize the text as best as you can in less than four paragraphs but atleast two paragraphs and when only include the summaraztion and nothing else:
+Also end the summary with by adding "END" and start with "START"
+
+Text: {text}
+
+Answer:""".format(text = text)
+
+        prompt_with_previous=  """Human: You are an assistant summarizing document text.
+Use the following pieces of retrieved context to add to the summary text. 
+If you can't add to it simply return the old.
+The new summary has to be at least two paragraphs long but never longer than three paragraphs of text.
+Dont Ever talk about improving the summary.
+Don't directly refer to the context text, pretend like you already knew the context information.
+
+
+Summary: {summary}
+
+Context: {context}
+
+Answer:""".format(summary = previous_summary,context=text)
+
+        use_prompt = prompt if previous_summary == None else prompt_with_previous
+        print(f"Summarizing doc {idx + 1}...")
+        print(f"Full prompt:")
+        print(use_prompt + "\n")
+        result: str = ""
+
+        # Start streaming the final summary only
+        if idx == len(docs['metadatas']) - 1:
+            for chunk in llm.stream(use_prompt):
+                result += chunk
+                yield chunk
+        else:
+            result = llm(use_prompt)
+        results.append(result)
+        texts = texts + text
+        
+
+    print("######################################\n\n\n")
+    for (idx, result) in enumerate(results):
+        print(f"Result {idx + 1}")
+        print(result + "\n\n\n")
+
+    print("################################\n")
+    print("Summary:")
+    summary = results[-1]
+    print("\n")
+    summaryTrim = summary[results[-1].find(start:='START')+len(start):summary.find('END')]
+    print(summaryTrim)
+    print("\n")
+    print("Original text:")
+    print(texts)
+    # return summaryTrim
 
 
 
