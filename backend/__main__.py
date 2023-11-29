28ebc74a9b8b89924c98533f4c15bbd5359df28d
Johan <jsbollll@gmail.com>
2023-11-29 13:06:11 +0100

    update chunks

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,20 +1,15 @@
 #from webserver.app  import start_app
 #from modules.ai.quizer import quiz_test
 
-from modules.files.chunks import chunkerizer
+from modules.files.chunks import Chunkerizer
 
 if __name__ == "__main__":
     #start_app()
     #quiz_test()
 
 
-    chunk = chunkerizer()
     path: str = "./backend/tests/sample_files/Test_pdfs/L01 Intro 2023 v02.pdf"
-    f = open(path, "rb")
-    bytes = f.read()
-    f.close()
 
-
-    tup = chunk.check_mimetype(bytes)
+    tup = Chunkerizer.text_and_image_text_from_file(path)
     print(tup[1])
     
