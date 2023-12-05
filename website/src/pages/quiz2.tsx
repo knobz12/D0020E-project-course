66f29616981d9a67bde0526008492d5c018a168b
Simon Ruskola <simon.ruskola@gmail.com>
2023-12-05 13:17:40 +0100

    fixed score bug

--- website/src/pages/quiz2.tsx
+++ website/src/pages/quiz2.tsx
@@ -1,7 +1,8 @@
 //import FileUpload from "@/components/FileUpload"
 //import React from "react"
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FileUpload from "@/components/FileUpload"
+import { sleep } from 'openai/core.mjs';
 
 //insperation 👌 https://www.geeksforgeeks.org/how-to-build-a-quiz-app-with-react-and-typescript/
 
@@ -64,13 +65,20 @@ const Question: React.FC<Props> = (
 };
 
 
-const Quiz: React.FC = () => {
+function Quiz(){
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
+    var TempScore = score;  
  
     const handleAnswer = (CorrectAnswer: string) => {
         if (CorrectAnswer === questions[currentQuestion].CorrectAnswer) {
-            setScore(score + 1);
+            console.log(TempScore);
+            TempScore = score + 1;
+            setScore(TempScore); // this is an asynchronous call so it wont update before score i shown
+            console.log(TempScore);
+
+            
+                
         }else{
             alert(`Wrong 🤢`);
         }
@@ -79,9 +87,9 @@ const Quiz: React.FC = () => {
         if (nextQuestion < questions.length) {
             setCurrentQuestion(nextQuestion);
         } else {
-            alert(`Quiz finished. You scored ${score}/${questions.length}`);
-            //reset
-            setScore(0);
+
+            alert(`Quiz finished. You scored ${TempScore}/${questions.length}`);
+            setScore(0); //reset quiz
             setCurrentQuestion(0);
         }
     };
@@ -109,11 +117,12 @@ const Quiz: React.FC = () => {
 export default function QuizSite() {
     return (
         <>
-        <FileUpload
+
+        {/* <FileUpload
             title=   "Generate quiz"
             apiUrl=  "http://localhost:3030/api/quiz"
             
-        /> 
+        />  */}
 
 
         <div className="">
