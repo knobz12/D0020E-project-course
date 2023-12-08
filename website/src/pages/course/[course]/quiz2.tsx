1d76485194cdc6e3c35491f6fff4bf008f33ea7e
Simon Ruskola <simon.ruskola@gmail.com>
2023-12-08 13:43:56 +0100

    got rid of duplicate file

--- website/src/pages/course/[course]/quiz2.tsx
+++ website/src/pages/course/[course]/quiz2.tsx
@@ -1,45 +1,69 @@
 //import FileUpload from "@/components/FileUpload"
 //import React from "react"
-import React, { useState } from "react"
+import React, { useState, useRef } from 'react';
 import FileUpload from "@/components/FileUpload"
+import { sleep } from 'openai/core.mjs';
+
+import {
+    Card,
+    Center,
+    Container,
+    Paper,
+    SimpleGrid,
+    Stack,
+    Text,
+    Title,
+} from "@mantine/core"
+import { Page } from "@/components/Page"
+import Link from "next/link"
+
 import { GetServerSideProps } from "next"
 import { getServerSession } from "next-auth"
 import { authOptions } from "../../api/auth/[...nextauth]"
 
 //insperation 👌 https://www.geeksforgeeks.org/how-to-build-a-quiz-app-with-react-and-typescript/
 
+
+
+
+
 const questions = [
     {
-        question: "What is the capital of France?",
-        choices: ["Paris", "London", "New York", "bollar"],
-        CorrectAnswer: "Paris",
+        question: "What is the boiling point of water? answer  1 or 2",
+        choices: ["100°C", "1°C", "2°C"],
+        CorrectAnswer: ["1°C","2°C"],
     },
     {
-        question: "What is the largest planet in our solar system?",
-        choices: ["Mars", "Jupiter", "Venus"],
-        CorrectAnswer: "Jupiter",
+        question: 'What is the capital of France?',
+        choices: ['Paris', 'London', 'bollar'],
+        CorrectAnswer: ['Paris'],
+    },
+    {
+        question: 'What is the largest planet in our solar system?',
+        choices: ['Jupiter','Mars',  'Venus'],
+        CorrectAnswer: ['Jupiter'],
     },
     {
         question: "What is the boiling point of water?",
         choices: ["100°C", "0°C", "50°C"],
-        CorrectAnswer: "100°C",
+        CorrectAnswer: ["100°C"],
     },
     {
-        question: "What is the largest planet in our solar system?",
-        choices: ["Mars", "Jupiter", "Venus"],
-        CorrectAnswer: "Jupiter",
+        question: 'What is the largest planet in our solar system?',
+        choices: ['Jupiter', 'Mars',  'Venus'],
+        CorrectAnswer: ['Jupiter'],
     },
     {
         question: "What is the boiling point of water?",
         choices: ["100°C", "0°C", "50°C,"],
-        CorrectAnswer: "100°C",
+        CorrectAnswer: ["100°C"],
     },
 ]
 
 interface Props {
     question: string
     choices: string[]
-    CorrectAnswer: string
+    CorrectAnswer: string[]
     onAnswer: (CorrectAnswer: string) => void
 }
 
@@ -73,23 +97,46 @@ const Question: React.FC<Props> = ({
         </div>
     )
 }
-
-const Quiz: React.FC = () => {
-    const [currentQuestion, setCurrentQuestion] = useState(0)
-    const [score, setScore] = useState(0)
-
-    const handleAnswer = (CorrectAnswer: string) => {
-        if (CorrectAnswer === questions[currentQuestion].CorrectAnswer) {
-            setScore(score + 1)
-        } else {
-            alert(`Wrong 🤢`)
+function CheckCorrectAnswer(answer:string, CorrectAnswer:string[]){
+    console.log(CorrectAnswer)
+    console.log(answer)
+    for(let i = 0; i < CorrectAnswer.length; i++){
+        console.log("dm"+i)
+        console.log(CorrectAnswer[i]+" and "+ answer)
+        if(CorrectAnswer[i] === answer){
+            console.log("correct")
+            return 1
         }
+    }
+    console.log("wrong")
+    return 0
+}
 
-        const nextQuestion = currentQuestion + 1
+function Quiz(){
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    var TempScore = score;  
+    
+    const handleAnswer = (CorrectAnswer: string) => {
+        if (CheckCorrectAnswer(CorrectAnswer, questions[currentQuestion].CorrectAnswer)===1){
+        //if (CorrectAnswer === questions[currentQuestion].CorrectAnswer[0]) {
+            console.log(TempScore);
+            TempScore = score + 1;
+            setScore(TempScore); // this is an asynchronous call so it wont update before score i shown
+            console.log(TempScore);
+                
+        }else{
+            alert(`Wrong 🤢`);
+        }
+ 
+        const nextQuestion = currentQuestion + 1;
         if (nextQuestion < questions.length) {
-            setCurrentQuestion(nextQuestion)
+            setCurrentQuestion(nextQuestion);
         } else {
-            alert(`Quiz finished. You scored ${score}/${questions.length}`)
+
+            alert(`Quiz finished. You scored ${TempScore}/${questions.length}`);
+            setScore(0);            //reset quiz
+            setCurrentQuestion(0);  //reset quiz
         }
     }
 
@@ -110,22 +157,45 @@ const Quiz: React.FC = () => {
     )
 }
 
+
+
 export default function QuizSite() {
+    var test = <FileUpload 
+    title=   "Generate quiz"
+    apiUrl=  "http://localhost:3030/api/quiz"
+    />
+
+    
+
+  
     return (
         <>
-            <FileUpload
-                title="Generate quiz"
-                apiUrl="http://localhost:3030/api/quiz"
-            />
-
-            <div className="">
-                <Quiz />
-            </div>
+        <Page center>
+            <Container w="100%" size="sm">
+                <Center w="100%" h="100%">
+                    <Stack w="100%">
+                        <Title>Quiz_test</Title>
+                        <Paper px="xl" py="lg">
+                            <Stack spacing="xl">
+                                <Stack>
+                                    <SimpleGrid cols={1}>
+                                        <div className="">
+                                            <Quiz />
+                                        </div>
+                                    </SimpleGrid>
+                                </Stack>
+                            </Stack>
+                        </Paper>
+                    </Stack>
+                </Center>
+            </Container>
+        </Page>
+            
         </>
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     const session = await getServerSession(req, res, authOptions)
 
     return {
@@ -133,4 +203,4 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
             session,
         },
     }
-}
+} 
