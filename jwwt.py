9b2e9b9ac3fc7388ea1edc7c565314907c630997
Knobz <mjojla@hotmail.com>
2023-12-11 14:54:35 +0100

    First commit

--- jwwt.py
+++ jwwt.py
@@ -1,6 +1,2 @@
-from jose import jwe
-x = jwe.encrypt('Jag har bara en 3700x :( sadface.)', 'This is the: 123', algorithm='dir', encryption='A128GCM')
 
-print(x)
-#jwe.decrypt('eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4R0NNIn0..McILMB3dYsNJSuhcDzQshA.OfX9H_mcUpHDeRM4IA.CcnTWqaqxNsjT4eCaUABSg', 'asecret128bitkey')
 #'Hello, World!'
