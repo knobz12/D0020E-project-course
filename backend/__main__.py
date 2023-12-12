d249d1ab17fe13a4161203f2e14a9cd06bbd11d4
Jonathan Ekberg <jodrim@gmail.com>
2023-12-12 12:35:59 +0100

    Better

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,27 +1,34 @@
-from webserver.app  import start_app
-from modules.ai.playground import run_llm
+def app():
+    from webserver.app  import start_app
+    start_app()
 
-from modules.files.chunks import Chunkerizer
+def playground():
+    from modules.ai.playground import run_llm
+    run_llm()
 
-from modules.ai.quizer import quiz_test
-
-if __name__ == "__main__":
-    # start_app()
-    # run_llm()
+def quiz():
+    from modules.ai.quizer import quiz_test
     quiz_test()
 
-    # path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//Application.pdf"
-    # path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//QualityAttributes.pdf"
-    # path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//2022_D7032E-04-Qualitative_attributes1.pptx"
+def chunky():
+    from modules.files.chunks import Chunkerizer
+    path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//Application.pdf"
+    path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//QualityAttributes.pdf"
+    path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//2022_D7032E-04-Qualitative_attributes1.pptx"
 
+    path: str = "./backend/tests/sample_files/Test_txt/Image15.txt"
 
-    #path: str = "./backend/tests/sample_files/Test_txt/Image15.txt"
+    Chunkerizer.text_extraction_test()
 
+    tup = Chunkerizer.text_and_image_text_from_file(path)
+    print(tup[1])
+    tup = Chunkerizer.text_and_image_text_from_file(path)
+    print(tup[1])
 
-    #Chunkerizer.text_extraction_test()
+if __name__ == "__main__":
+    app()
+    # playground()
+    # quiz()
+    # chunky()
 
-    # tup = Chunkerizer.text_and_image_text_from_file(path)
-    # print(tup[1])
-    # tup = Chunkerizer.text_and_image_text_from_file(path)
-    # print(tup[1])
     
