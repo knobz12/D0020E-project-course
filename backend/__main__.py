82a7787079b1489901c406aa7c0a676f055e5fcf
Johan <jsbollll@gmail.com>
2023-12-12 12:45:59 +0100

    remove unused

--- backend/__main__.py
+++ backend/__main__.py
@@ -12,18 +12,7 @@ def quiz():
 
 def chunky():
     from modules.files.chunks import Chunkerizer
-    path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//Application.pdf"
-    path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//QualityAttributes.pdf"
-    path: str = "C://dev//projects//python//D0020E-project-course//backend//tests//sample_files//courses//D7032E//2022_D7032E-04-Qualitative_attributes1.pptx"
-
-    path: str = "./backend/tests/sample_files/Test_txt/Image15.txt"
-
     Chunkerizer.text_extraction_test()
-
-    tup = Chunkerizer.text_and_image_text_from_file(path)
-    print(tup[1])
-    tup = Chunkerizer.text_and_image_text_from_file(path)
-    print(tup[1])
 
 if __name__ == "__main__":
     app()
