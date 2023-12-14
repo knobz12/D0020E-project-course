e46bf7475de05e841df760a6a6ef0da567013e83
Jonathan Ekberg <jodrim@gmail.com>
2023-12-14 12:10:57 +0100

    good

--- backendV2/llama_runner.py
+++ backendV2/llama_runner.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 llm = LlamaCPP(
     # You can pass in the URL to a GGML model to download it automatically
-    model_path="/home/knobz/Documents/D0020E/D0020E-project-course/backend/models/default_.Q5_K_M.gguf",
+    model_path="E:/models/zephyr-7b-beta.Q6_K.gguf",
     # optionally, you can set the path to a pre-downloaded model instead of model_url
     temperature=0,
     max_new_tokens=512,
@@ -27,31 +27,67 @@ llm = LlamaCPP(
     generate_kwargs={},
     # kwargs to pass to __init__()
     # set to at least 1 to use GPU
-    model_kwargs={"n_gpu_layers": 25, "use_mmap": True, "f16_kv": True},
+    model_kwargs={"n_gpu_layers": 33, "use_mmap": True, "f16_kv": True},
     # transform inputs into Llama2 format
     messages_to_prompt=messages_to_prompt,
     completion_to_prompt=completion_to_prompt,
     verbose=True
 )
 
-#llm_predictor = LLMPredictor(llm=llm)
+llm_predictor = LLMPredictor(llm=llm)
 
-"""service_context = ServiceContext.from_defaults(
-    chunk_size=512,
-    llm=llm,
-    #llm_predictor=llm_predictor,
-    embed_model='local'
-)"""
 service_context = ServiceContext.from_defaults(
     chunk_size=512,
-    llm="local"
+    llm=llm,
+    # llm_predictor=llm_predictor,
+    embed_model='local'
 )
-#set_global_service_context(service_context)
+"""service_context = ServiceContext.from_defaults(
+    chunk_size=512,
+    llm="local"
+)"""
+set_global_service_context(service_context)
 
-documents = SimpleDirectoryReader("/home/knobz/Documents/D0020E/D0020E-project-course/backend/tests/sample_files/Test_pdfs").load_data()
+documents = SimpleDirectoryReader("./backend/tests/sample_files/Test_pdfs").load_data()
+# print(documents)
+# print(len(documents))
+# print(documents[0])
+# exit(0)
+
+import fasttext
+from huggingface_hub import hf_hub_download
+
+model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
+model = fasttext.load_model(model_path)
+good_labels = ["__label__swe_Latn", "__label__eng_Latn"]
+from llama_index import Document
+goods: list[Document] = []
+
+for doc in documents:
+    # print(doc)
+    # print(type(doc))
+    orig_text: str = doc.text
+    text:str = doc.text
+    text = text.replace("\n", "")
+    if len(text) < 50:
+        continue
+
+    print("Checking:", text)
+    prediction = model.predict(text, k=3)
+    for pred in prediction:
+        if good_labels.__contains__(pred[0]):
+            goods.append(doc)
+            print("Adding text")
+            break
+
+    print(prediction)
+# model.predict("Hello, world!")
+print("Using:",goods)
+# exit(0)
 #index = VectorStoreIndex.from_documents(documents)
 
-data_generator = DatasetGenerator.from_documents(documents=documents, service_context=service_context)
+qst = ("The questions should be regular quiz type questions where the question is about the theory. Don't make questions about files or properties of files.")
+data_generator = DatasetGenerator.from_documents(question_gen_query=qst, documents=goods[0:5], service_context=service_context)
 eval_questions = data_generator.generate_questions_from_nodes()
 print(eval_questions)
 
