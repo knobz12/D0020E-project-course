bdd0057b62fef370baef357190c2fede498b43f2
Knobz <mjojla@hotmail.com>
2024-01-15 11:23:14 +0100

    Testing openai api

--- backendV2/llama_runner.py
+++ backendV2/llama_runner.py
@@ -1,4 +1,9 @@
-from llama_index.llms import LlamaCPP
+import os
+with open("/home/knobz/Documents/D0020E/D0020E-project-course/backendV2/secret.txt", "r") as file:
+    api_key = file.read()
+    print(api_key)
+os.environ["OPENAI_API_KEY"] = api_key
+from llama_index.llms import LlamaCPP, OpenAI
 from llama_index.llms.llama_utils import messages_to_prompt, completion_to_prompt
 from llama_index.node_parser import SimpleNodeParser
 from llama_index.langchain_helpers.text_splitter import TokenTextSplitter
@@ -15,6 +20,7 @@ from llama_index.evaluation import DatasetGenerator, RelevancyEvaluator
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+#test_llm = LLMPredictor(OpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=200))
 llm = LlamaCPP(
     # You can pass in the URL to a GGML model to download it automatically
     model_path="/home/knobz/Documents/D0020E/D0020E-project-course/backend/models/default_.Q5_K_M.gguf",
@@ -39,14 +45,10 @@ llm = LlamaCPP(
 service_context = ServiceContext.from_defaults(
     chunk_size=512,
     llm=llm,
-    #llm_predictor=llm_predictor,
     embed_model='local'
 )
-"""
-service_context = ServiceContext.from_defaults(
-    chunk_size=512,
-    llm="local"
-)"""
+
+
 set_global_service_context(service_context)
 
 documents = SimpleDirectoryReader("/home/knobz/Documents/D0020E/D0020E-project-course/backend/tests/sample_files/Test_pdfs").load_data()
@@ -58,5 +60,5 @@ print(eval_questions)"""
 
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine(streaming=True,similarity_top_k=3)
-response_stream = query_engine.query("Your job is to make quizzes. Make 5 quiz questions about the documents. Each question should have only 4 answer choices each and each new question can't have answers from previous questions, only new answers.Only one answer for each question should be correct. Mark which answers are correct and which answers are false. Give the output in json format.")
+response_stream = query_engine.query("Your job is to make quizzes. Make 5 quiz questions about the documents. Each question should have only 4 answer choices each and each new question can't have answers from previous questions, only new answers.Only one answer for each question should be correct. Mark which answers are correct and which answers are false. Give the output in json format.", LLMPredictor=test_llm)
 response_stream.print_response_stream()
