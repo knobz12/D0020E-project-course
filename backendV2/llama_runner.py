7870041be8317e37a2948767043f50430c3b75b1
Knobz <mjojla@hotmail.com>
2024-01-16 12:21:00 +0100

    Update llama_runner.py

--- backendV2/llama_runner.py
+++ backendV2/llama_runner.py
@@ -1,8 +1,9 @@
-import os
+"""import os
 with open("/home/knobz/Documents/D0020E/D0020E-project-course/backendV2/secret.txt", "r") as file:
     api_key = file.read()
     print(api_key)
-os.environ["OPENAI_API_KEY"] = api_key
+os.environ["OPENAI_API_KEY"] = api_key"""
+
 from llama_index.llms import LlamaCPP, OpenAI
 from llama_index.llms.llama_utils import messages_to_prompt, completion_to_prompt
 from llama_index.node_parser import SimpleNodeParser
@@ -51,7 +52,7 @@ service_context = ServiceContext.from_defaults(
 
 set_global_service_context(service_context)
 
-documents = SimpleDirectoryReader("/home/knobz/Documents/D0020E/D0020E-project-course/backend/tests/sample_files/Test_pdfs").load_data()
+documents = SimpleDirectoryReader("/home/knobz/Documents/D0020E/D0020E-project-course/backend/tests/sample_files/Test_htmls").load_data()
 
 
 """data_generator = DatasetGenerator.from_documents(documents=documents, service_context=service_context)
@@ -59,6 +60,9 @@ eval_questions = data_generator.generate_questions_from_nodes()
 print(eval_questions)"""
 
 index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine(streaming=True,similarity_top_k=3)
-response_stream = query_engine.query("Your job is to make quizzes. Make 5 quiz questions about the documents. Each question should have only 4 answer choices each and each new question can't have answers from previous questions, only new answers.Only one answer for each question should be correct. Mark which answers are correct and which answers are false. Give the output in json format.", LLMPredictor=test_llm)
+query_engine = index.as_query_engine(streaming=True,similarity_top_k=3, service_context=service_context)
+#response_stream = query_engine.query("Your job is to make quizzes. Make 5 quiz questions about the documents. Each question should have only 4 answer choices each and each new question can't have answers from previous questions, only new answers.Only one answer for each question should be correct. Mark which answers are correct and which answers are false. Give the output in json format.", LLMPredictor=test_llm)
+response_stream = query_engine.query("Pick out 10 keywords that are important across the documents. For each keyword write a short explaination. Give the output in json format")
+
+#print(response_stream)
 response_stream.print_response_stream()
