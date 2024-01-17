8a7a37ef0c1981516b8d7aacc1746640020b5d05
Johan <jsbollll@gmail.com>
2024-01-17 11:16:56 +0100

    fix style of flashcards

--- website/src/pages/course/[course]/playableFlashcards.tsx
+++ website/src/pages/course/[course]/playableFlashcards.tsx
@@ -2,7 +2,6 @@
 //import React from "react"
 import React, { useState, useRef } from 'react';
 import FileUpload from "@/components/FileUpload"
-import { sleep } from 'openai/core.mjs';
 
 
 import {
@@ -29,7 +28,7 @@ import { authOptions } from "../../api/auth/[...nextauth]"
 import { useRouter } from "next/router"
 import { RouterOutput, trpc } from "@/lib/trpc"
 
-//insperation 👌 https://www.geeksforgeeks.org/how-to-build-a-flashcards-app-with-react-and-typescript/
+
 
 type Content = (RouterOutput["prompts"]["getPromptById"] & { type: "FLASHCARDS" })["content"]
 
@@ -42,6 +41,7 @@ export default function Flashcards(Flashcardsquestions:Content){
 
     let [data] = useState<Content >(Flashcardsquestions)
     let [current, setCurrent] = useState(0)
+    let [flipped, setFlipped] = useState(false)
 
     function previousCard() {
         setCurrent(current - 1);
@@ -50,44 +50,58 @@ export default function Flashcards(Flashcardsquestions:Content){
         setCurrent(current + 1);
     }
 
-
+    function toggleFlipped () {
+        setFlipped(!flipped)
+    }
     
+
     return (
-        <div>
-        {/* number of cards */}
-        {data && data.questions.length > 0 ? (
-            <div className="cardNumber">
-            Card {current + 1} of {data.questions.length}
+        <div className="grid place-items-center h-full min-h-screen">
+            <div>
+                {/* number of cards */}
+                {data && data.questions.length > 0 ? (
+                    <div className="cardNumber">
+                    Card {current + 1} of {data.questions.length}
+                    </div>
+                ) : (
+                    ""
+                )}
+                {/* /number of cards */}
+
+                {/* render cards */}
+                <div className="card">
+                    <div onClick={toggleFlipped} style={{transform: flipped ? "rotateY(180deg)" : "rotateY(0deg)"}}className="card_inner">
+                        <div className="card_front">
+                            {data && data.questions.length > 0 ? data.questions[current].question : ""}
+                        </div>
+                        <div className="card_back">
+                            {data && data.questions.length > 0 ? data.questions[current].answer : ""}
+                        </div>
+                    </div>
+                </div>
+                {/* /render cards */}
+
+                {/* render nav buttons */}
+                <div className="nav">
+                    {current > 0 ? (
+                    <button onClick={previousCard}>Previous card</button>
+                    ) : (
+                    <button className="disabled" disabled>
+                        Previous card
+                    </button>
+                    )}
+                    {data && current < data.questions.length - 1 ? (
+                    <button onClick={nextCard}>Next card</button>
+                    ) : (
+                    <button className="disabled" disabled>
+                        Next card
+                    </button>
+                    )}
+                    {/* /render nav buttons */}
+                </div>
             </div>
-        ) : (
-            ""
-        )}
-        {/* /number of cards */}
 
-        {/* render cards */}
-        {data && data.questions.length > 0 ? data.questions[current].question : ""}
-        {data && data.questions.length > 0 ? data.questions[current].answer : ""}
-        {/* /render cards */}
-
-        {/* render nav buttons */}
-        <div className="nav">
-            {current > 0 ? (
-            <button onClick={previousCard}>Previous card</button>
-            ) : (
-            <button className="disabled" disabled>
-                Previous card
-            </button>
-            )}
-            {data && current < data.questions.length - 1 ? (
-            <button onClick={nextCard}>Next card</button>
-            ) : (
-            <button className="disabled" disabled>
-                Next card
-            </button>
-            )}
-            {/* /render nav buttons */}
-        </div>
-        </div>
+            </div>
     );
 }
 
