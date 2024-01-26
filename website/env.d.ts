8c2346bfaf06ae56d0f80bdee9cef4cef7a6c0f6
Jonathan Ekberg <jodrim@gmail.com>
2024-01-26 12:52:22 +0100

    Update

--- website/env.d.ts
+++ website/env.d.ts
@@ -1,5 +1,16 @@
 namespace NodeJS {
     interface ProcessEnv {
+        // Browser and server
         NEXT_PUBLIC_API_URL: string
+
+        // Only server
+        NEXTAUTH_URL: string
+        NEXTAUTH_SECRET: string
+        CHROMA_URL: string
+        DATABASE_URL: string
+        GITHUB_ID: string
+        GITHUB_SECRET: string
+        GOOGLE_ID: string
+        GOOGLE_SECRET: string
     }
 }
