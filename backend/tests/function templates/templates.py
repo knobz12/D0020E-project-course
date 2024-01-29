115572f8fca0bc1b5af51b1fb581151ab413aceb
Johan <jsbollll@gmail.com>
2024-01-29 11:50:36 +0100

    remove unused

--- backend/tests/function templates/templates.py
+++
@@ -1,5 +0,0 @@
-#Cuatom error
-
-class NotSupportedFiletype(Exception):
-    def __init__(self, message = "Type not currently supported. We are working on a fix in later versions."):
-        super().__init__(message)
