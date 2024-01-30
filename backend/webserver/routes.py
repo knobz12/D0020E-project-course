751db48a4e5ab7dbb1a69afdbfa9ff6eff373bf0
Knobz <mjojla@hotmail.com>
2024-01-30 11:46:06 +0100

    Fixed explainer ish

--- backend/webserver/routes.py
+++ backend/webserver/routes.py
@@ -290,7 +290,7 @@ def generate_title():
 
     content: str = (str(prompt[0]))#[0:4096]
     #title: str = create_title(content)
-    title: str = create_title_index(content) + " " + str(datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    title: str = create_title_index(content) + " " + str(datetime.datetime.today().strftime('%Y-%m-%d'))
 
     cur.execute("UPDATE prompts SET title=%s WHERE id=%s;", (title, prompt_id))
     conn.commit()
