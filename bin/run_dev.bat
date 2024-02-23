1e96c9c7b73e96011873d624758e4f73a88ce20a
Johan <jsbollll@gmail.com>
2024-02-23 20:43:01 +0100

    fix db creation

--- bin/run_dev.bat
+++ bin/run_dev.bat
@@ -12,10 +12,10 @@ SET POSTGRES_BIN=%cd%/postgres/pgsql/bin
 SET CHROMA_DIR=%cd%/chroma/chroma-0.4.22
 
 mkdir post_data
-%POSTGRES_BIN%/initdb.exe -D %cd%/post_data
+%POSTGRES_BIN%/initdb.exe --username=user -D %cd%/post_data 
 start "postgres" "%POSTGRES_BIN%/postgres.exe" -D %cd%/post_data
 timeout /t 1
-%POSTGRES_BIN%/createuser.exe -s user
+%POSTGRES_BIN%/createdb.exe db 
 
 call %cd%/venv_chroma/Scripts/activate.bat
 cd chroma/chroma-0.4.22
@@ -38,7 +38,7 @@ SET GOOGLE_ID=******************************************************************
 SET GOOGLE_SECRET=************************************
 
 @rem # Should work as is but you may change
-SET DATABASE_URL="postgresql://user:pass@localhost:5432/db?schema=public"
+SET DATABASE_URL=postgresql://user:pass@localhost:5432/db?schema=public
 SET NEXT_PUBLIC_API_URL=http://localhost:3030
 SET COOKIE_DOMAIN=localhost
 SET CHROMA_URL=http://localhost:8000
@@ -49,5 +49,6 @@ SET JWE_SECRET=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 SET JWE_PEPPER=BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
 SET NEXTAUTH_SECRET=123
 
-
+timeout /t 1
+%PNPM% prisma migrate reset -f --skip-seed && %PNPM% prisma db push && %PNPM% prisma db seed
 start "node" "%NODE%" server.js -p 3000
