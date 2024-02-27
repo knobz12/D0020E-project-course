2560c03d0e3cf17b9391be52a8a117d1b3a6dd14
Jonathan Ekberg <jodrim@gmail.com>
2024-02-27 12:26:25 +0100

    Fix order

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -15,36 +15,43 @@ export default function LandingPage({}: LandingPageProps) {
     return (
         <Page>
             {/* <Box> */}
-            <BackgroundBeams />
-            <div className="relative px-6 pt-14 lg:px-8">
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-6xl">
-                            The worlds best AI studying assistant
-                        </h1>
-                        <p className="mt-6 text-lg leading-8 text-gray-400">
-                            Anim aute id magna aliqua ad ad non deserunt sunt.
-                            Qui irure qui lorem cupidatat commodo. Elit sunt
-                            amet fugiat veniam occaecat fugiat aliqua.
-                        </p>
-                        <Group position="center">
-                            <Link href={"/courses"} passHref legacyBehavior>
-                                <Button component="a" size="lg" variant="white">
-                                    View app
-                                </Button>
-                            </Link>
-                            {auth.status !== "authenticated" && (
-                                <Link
-                                    href="/api/auth/signin"
-                                    passHref
-                                    legacyBehavior
-                                >
-                                    <Button component="a" size="lg">
-                                        Log in
+            <div className="relative">
+                <BackgroundBeams />
+                <div className="relative px-6 pt-14 lg:px-8">
+                    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                        <div className="text-center">
+                            <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-6xl">
+                                The worlds best AI studying assistant
+                            </h1>
+                            <p className="mt-6 text-lg leading-8 text-gray-400">
+                                Anim aute id magna aliqua ad ad non deserunt
+                                sunt. Qui irure qui lorem cupidatat commodo.
+                                Elit sunt amet fugiat veniam occaecat fugiat
+                                aliqua.
+                            </p>
+                            <Group position="center">
+                                <Link href={"/courses"} passHref legacyBehavior>
+                                    <Button
+                                        component="a"
+                                        size="lg"
+                                        variant="white"
+                                    >
+                                        View app
                                     </Button>
                                 </Link>
-                            )}
-                        </Group>
+                                {auth.status !== "authenticated" && (
+                                    <Link
+                                        href="/api/auth/signin"
+                                        passHref
+                                        legacyBehavior
+                                    >
+                                        <Button component="a" size="lg">
+                                            Log in
+                                        </Button>
+                                    </Link>
+                                )}
+                            </Group>
+                        </div>
                     </div>
                 </div>
             </div>
