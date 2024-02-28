d795db2fb5dd4392e2c3231af1c8b267dd6bbcb7
Jonathan Ekberg <jodrim@gmail.com>
2024-02-28 13:01:27 +0100

    Fix colors on light mode

--- website/src/components/Beams.tsx
+++ website/src/components/Beams.tsx
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function BackgroundBeams() {
     return (
         <div id="beams" className="fixed top-0 left-0 w-full h-full -z-10">
-            <div className="h-full w-full dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center -z-10">
+            <div className="h-full w-full bg-dot-white/[0.2] relative flex items-center justify-center -z-10">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black/70 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             </div>
         </div>
